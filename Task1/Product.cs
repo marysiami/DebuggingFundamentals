6d260b5ec4d9e08bc5a8aa73b02c82b7138946b8
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-03-01 13:00:26 +0100

    fix

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -1,23 +1,25 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     public class Product
     {
+        public string Name { get; }
+
+        public double Price { get; }
+
         public Product(string name, double price)
         {
             Name = name;
             Price = price;
         }
 
-        public string Name { get; set; }
-
-        public double Price { get; set; }
-
         public override bool Equals(object obj)
         {
             if (!(obj is Product other))
                 return false;
 
-            if (Name == other.Name && Price == other.Price)
+            if (Name == other.Name && Math.Abs(Price - other.Price) < 0.0000001)
                 return true;
 
             return false;
@@ -25,7 +27,7 @@
 
         public override int GetHashCode()
         {
-            return this.Name.GetHashCode();
+            return HashCode.Combine(Name, Price);
         }
     }
 }
